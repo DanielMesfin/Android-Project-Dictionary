a9f4ed2c862306039284fe0a3f02d3af9c3ec09c
beke1221 <attobekalu@gmail.com>
2022-07-28 09:49:01 +0300

    final

---
+++ app/src/main/java/com/example/amharicorommifa/MainActivity.java
@@ -0,0 +1,214 @@
+package com.example.amharicorommifa;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.text.TextUtilsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.ColorStateList;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView list;
+    customAdapter adapter,adapterAmh;
+    String[] title;
+    String[] desc;
+    ArrayList<model> arrayList = new ArrayList<model>();
+    ArrayList<model> arrayListAmh=new ArrayList<model>();
+
+    SearchView search;
+
+    TextView amh,orom;
+    ImageView swap;
+     int s = 0;
+     private boolean Wchanged=false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        amh=findViewById(R.id.amharic);
+        orom=findViewById(R.id.orommiffa);
+        swap = findViewById(R.id.swap);
+
+        search = (SearchView) findViewById(R.id.searchamharic);
+
+        title = getResources().getStringArray(R.array.oromiffa_words);
+        desc = getResources().getStringArray(R.array.amharic_word);
+
+        list = findViewById(R.id.lv);
+
+        for (int i = 0; i < title.length; i++) {
+                        model model = new model(title[i], desc[i]);
+                        model modelAmh = new model(desc[i],title[i]);
+                        arrayList.add(model);
+                       arrayListAmh.add(modelAmh);
+                    }
+        //pass results to listview adapter class
+        adapter = new customAdapter(MainActivity.this,arrayList);
+        adapterAmh=new customAdapter(MainActivity.this,arrayListAmh);
+
+        list.setAdapter(adapter);
+
+        swap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                swapped();
+                if (s == 0) {
+                    //bind the adapter to the listview
+                    list.setAdapter(adapter);
+                    amh.setText("Oromiffa");
+                    orom.setText("አማረኛ");
+                    search.setQueryHint("Afaan oromiffatti barbaadi ....");
+
+                    setisWchanged(true);
+
+                } else {
+                    //bind the adapter to the listview
+                    list.setAdapter(adapterAmh);
+                    orom.setText("Oromiffa");
+                    amh.setText("አማረኛ");
+                    search.setQueryHint("በ አማረኛ ይፈልጉ ....");
+                    setisWchanged(false);
+                }
+
+            }
+        });
+
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                if (TextUtils.isEmpty(s)) {
+                    adapter.filter("");
+                    list.clearTextFilter();
+                } else {
+                    adapter.filter(s);
+                }
+
+
+                return true;
+            }
+        });
+
+        BottomNavigationView bottomview = findViewById(R.id.bottom_nav);
+        bottomview.setOnNavigationItemSelectedListener(navlistener);
+
+    }
+
+    public void setisWchanged(boolean wchanged){
+        this.Wchanged=wchanged;
+    }
+    public boolean getWchanged(){
+        return this.Wchanged;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        MenuItem myactionmenuItem = menu.findItem(R.id.action_search);
+
+        SearchView searchView = (SearchView) myactionmenuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+
+                if (TextUtils.isEmpty(s)) {
+                    adapter.filter( s);
+                    list.clearTextFilter();
+                } else {
+                    adapter.filter(s);
+                }
+                return true;
+            }
+
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+
+            //TODO functionality to be defined here
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navlistener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    Fragment selected = null;
+
+                    switch (item.getItemId()) {
+
+                        case R.id.home:
+                            break;
+                        case R.id.about:
+                            Intent intentAbout = new Intent(MainActivity.this,About.class);
+                            startActivity(intentAbout);
+
+                            break;
+                        case R.id.favv:
+                            findViewById(R.id.favv).setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.Red)));
+                            Intent intentFavourite = new Intent(MainActivity.this,Favourites.class);
+                            startActivity(intentFavourite);
+                            break;
+                    }
+                    return true;
+                }
+            };
+
+    public int swapped(){
+                if(s==0){
+                   this.s=1;
+                }else {
+                    this.s=0;
+                }
+   return this.s;
+    }
+}
---
+++ app/src/main/java/com/example/amharicorommifa/customAdapter.java
@@ -0,0 +1,232 @@
+package com.example.amharicorommifa;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class customAdapter  extends BaseAdapter{
+
+   Context mcontext;
+   LayoutInflater inflater;
+   List<model> modellist;
+   ArrayList<model> arrayList;
+
+   ////////////////////////////////////
+   ArrayList<model> arrayListAmh;
+   List<model> modelListAmh;
+   /////////////////////////////////
+
+   String mainword="";
+   String wdefinition="";
+
+    MainActivity activity;
+    Resources res;
+
+    public  customAdapter(Context context,List<model> modellist){
+
+       mcontext=context;
+       this.modellist=modellist;
+       inflater=LayoutInflater.from(context);
+       this.arrayList=new ArrayList<model>();
+       this.arrayList.addAll(modellist);
+       this.modelListAmh=modellist;  ////////////////////////
+        this.arrayListAmh=new ArrayList<model>();  //////////////////
+        this.arrayListAmh.addAll(modelListAmh);  //////////////////////
+   }
+   public  class  ViewHolder { TextView mtitleTv,mDescTv;ImageView fav;}
+
+    @Override
+    public int getCount() { return modellist.size(); }
+
+    @Override
+    public Object getItem(int i) {
+        return modellist.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int Position, View view, ViewGroup parent) {
+       ViewHolder holder;
+       if(view==null){
+           holder=new ViewHolder();
+           view=inflater.inflate(R.layout.row,null);
+
+           //locate the views in row .xml
+           holder.mtitleTv=view.findViewById(R.id.maintitle);
+           holder.mDescTv=view.findViewById(R.id.subtitle);
+
+           view.setTag(holder);
+       }
+       else{
+           holder=(ViewHolder) view.getTag();
+       }
+            //set the results into textviews
+            holder.mtitleTv.setText(modellist.get(Position).getTitle());
+            holder.mDescTv.setText(modellist.get(Position).getDesc());
+
+      holder.fav=view.findViewById(R.id.favourite12);
+
+       res= view.getResources();
+       holder.fav.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+
+               if (holder.fav.getDrawable().getConstantState() == view.getResources().getDrawable( R.drawable.ic_baseline_favorite_border_24).getConstantState()) {
+
+                   holder.fav.setImageDrawable(res.getDrawable(R.drawable.ic_baseline_favorite_24red));
+               }
+               else {
+                   holder.fav.setImageDrawable(res.getDrawable(R.drawable.ic_baseline_favorite_border_24));
+               }
+           }
+       });
+
+       //listview item clicks
+       view.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               //TODO to be completed later
+
+               view.setOnClickListener(new View.OnClickListener() {
+                   @Override
+                   public void onClick(View view) {
+
+                       //TODO CHECK LATER   // gets the seleccted list item and toasts to user
+
+                       mainword=modellist.get(Position).getTitle();
+                       wdefinition=modellist.get(Position).getDesc();
+
+                       MainActivity activity = new MainActivity();
+
+                       BottomSheetDialog bsd = new BottomSheetDialog(mcontext,R.style.BottomSheetDialogTheme);
+
+                       View bsv = LayoutInflater.from(mcontext).inflate(R.layout.layout_bottom_sheet,(LinearLayout)view.findViewById(R.id.bottom_sheetContainer));
+                       TextView mainText=bsv.findViewById(R.id.mainWord);
+                       TextView defText=bsv.findViewById(R.id.definitionWord);
+
+                       TextView amhBanner = bsv.findViewById(R.id.amharicBanner);
+                       TextView orBanner = bsv.findViewById(R.id.orommiffaBanner);
+
+                       ImageView fav=bsv.findViewById(R.id.favourite);
+                       ImageView fav_def=bsv.findViewById(R.id.favourite1);
+
+                       fav_def.setOnClickListener(new View.OnClickListener() {
+                           @Override
+                           public void onClick(View view) {
+                               if (holder.fav.getDrawable().getConstantState() == view.getResources().getDrawable( R.drawable.ic_baseline_favorite_border_24).getConstantState()) {
+
+                                   holder.fav.setImageDrawable(res.getDrawable(R.drawable.ic_baseline_favorite_24red));
+                                   fav.setImageDrawable(res.getDrawable(R.drawable.ic_baseline_favorite_24red));
+                                   fav_def.setImageDrawable(res.getDrawable(R.drawable.ic_baseline_favorite_24red));
+                               }
+                               else {
+                                   holder.fav.setImageDrawable(res.getDrawable(R.drawable.ic_baseline_favorite_border_24));
+                                   fav.setImageDrawable(res.getDrawable(R.drawable.ic_baseline_favorite_border_24));
+                                   fav_def.setImageDrawable(res.getDrawable(R.drawable.ic_baseline_favorite_border_24));
+                               }
+                           }
+                       });
+
+                       fav.setOnClickListener(new View.OnClickListener() {
+                           @Override
+                           public void onClick(View view) {
+
+
+                               if (holder.fav.getDrawable().getConstantState() == view.getResources().getDrawable( R.drawable.ic_baseline_favorite_border_24).getConstantState()) {
+
+                                   holder.fav.setImageDrawable(res.getDrawable(R.drawable.ic_baseline_favorite_24red));
+                                   fav.setImageDrawable(res.getDrawable(R.drawable.ic_baseline_favorite_24red));
+                                   fav_def.setImageDrawable(res.getDrawable(R.drawable.ic_baseline_favorite_24red));
+                               }
+                               else {
+                                   holder.fav.setImageDrawable(res.getDrawable(R.drawable.ic_baseline_favorite_border_24));
+                                   fav.setImageDrawable(res.getDrawable(R.drawable.ic_baseline_favorite_border_24));
+                                   fav_def.setImageDrawable(res.getDrawable(R.drawable.ic_baseline_favorite_border_24));
+                               }
+
+
+
+                           }
+                       });
+                       mainText.setText(mainword);
+                       defText.setText(wdefinition);
+
+                       bsd.setContentView(bsv);
+                       bsd.show();
+                   }
+               });
+           }
+       });
+
+        return view;
+    }
+
+    public  void  filter(String charText){
+       charText = charText.toLowerCase(Locale.getDefault());
+       modellist.clear();
+
+       if(charText.length()==0){
+           modellist.addAll(arrayList);
+       }
+       else{
+           for(model model : arrayList){
+               if(model.getTitle().toLowerCase(Locale.getDefault())
+                       .contains(charText)){
+                   modellist.add(model);
+               }
+           }
+       }
+       notifyDataSetChanged();
+    }
+    public  void  filterAm(String charText){
+
+        charText = charText.toLowerCase(Locale.getDefault());
+        modelListAmh.clear();
+
+        if(charText.length()==0){
+            modelListAmh.addAll(arrayListAmh);
+        }
+        else{
+            for(model model : arrayListAmh){
+                if(model.getTitle().toLowerCase(Locale.getDefault())
+                        .contains(charText)){
+                    modelListAmh.add(model);
+                }
+            }
+        }
+        notifyDataSetChanged();
+    }
+}
+
